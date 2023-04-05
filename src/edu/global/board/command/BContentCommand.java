eea75b2d68219567340517696c95cf852a8924cf
wooorim <woorim0923@gmail.com>
2023-04-05 09:31:56 +0900

    commit

--- src/edu/global/board/command/BContentCommand.java
+++ src/edu/global/board/command/BContentCommand.java
@@ -18,5 +18,7 @@ public class BContentCommand implements BCommand{
 		BoardVO vo = dao.contentView(bid);
 		
 		request.setAttribute("content_view", vo);
+		//request객체는 forwarding가 끝날 때까지 정보가 남아있고, request객체에는 DB정보가 담겨져있다.
+		//DB정보를 jsp파일에 넘기기위해서는 .do로 전달할 수 밖에 없다.
 	}
 }
